da42ff7a9b54c9ad0179aea7b2d86ce2dec4d315
enjoroge <njrogeelizabeth53@gmail.com>
2024-06-27 22:07:26 +0300

    complete

--- lib/testing/ipdb_debugging_test.py
+++ lib/testing/ipdb_debugging_test.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python3
-
+import pytest
 from ipdb_debugging import plus_two
 
 class TestIpdbDebugging:
-    '''ipdb_debugging.py'''
-    
+    '''Test case for ipdb_debugging.py'''
+
     def test_adds_two(self):
         '''adds_two() adds 2 to input arg and returns sum.'''
-        assert(plus_two(3) == 5)
+        import ipdb; ipdb.set_trace()  # Debugging point
+        result = plus_two(3)
+        assert result == 5
